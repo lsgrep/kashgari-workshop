30a78be8c4e6f1c364d3fddac14eed82ad1ea449
Yusup <awklsgrep@gmail.com>
2019-12-14 03:28:06 +0800

    set things up

---
+++ README.md
@@ -0,0 +1,6 @@
+### Kashgari Workshop
+
+1. run `prep.sh`, get the dependencies and data ready.
+2. `python3 classification.py`, train & save model
+3. run `docker.sh` to serve the model
+4. run `client.py` to test if server is working correctly
--- prep.sh
+++ prep.sh
@@ -3,6 +3,8 @@ wget https://storage.googleapis.com/bert_models/2018_11_03/chinese_L-12_H-768_A-
 unzip chinese_L-12_H-768_A-12.zip
 
 wget https://lsgrep.com/weibo_senti_100k.csv
+
+
 sudo pip3 install pandas kashgari tensorflow-gpu==1.14.0
 
-sudo pip3 install --upgrade --force-reinstall scipy
+sudo pip3 install --upgrade --force-reinstall scipy jieba
