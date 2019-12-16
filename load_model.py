4f858c95415d5787d6464b6fb2304623f1c15219
Yusup <awklsgrep@gmail.com>
2019-12-16 18:31:10 +0800

    cleanup

--- load_model.py
+++
@@ -1,6 +0,0 @@
-import jieba
-import kashgari
-
-model = kashgari.utils.load_model('bert_model')
-random_stuff = ['好高兴', '好欢乐', '心情低落', '质量好差']
-print(model.predict([list(jieba.cut(i)) for i in random_stuff]))
